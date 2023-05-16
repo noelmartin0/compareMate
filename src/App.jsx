69f6799f0476526be5e9b11badab713145de4617
Noel Martin <noelmartin0@gmail.com>
2023-05-16 15:20:27 +0530

    need to fix css error

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import Navbar from "./components/navbar"
+import Login from "./pages/Login"
+import Home from "./pages/Home"
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 const App = () => {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/login' element={<Login/>} />
+      </Routes>
+    </Router>
   )
 }
 
