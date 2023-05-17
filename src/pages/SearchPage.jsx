4abe036429252e338b18849547f458c5da03600a
Sharun Raj Nambayil <sharunraj39@gmail.com>
2023-05-17 19:58:01 +0530

    Created footer and Results page

---
+++ src/pages/SearchPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/navbar'
+const SearchPage = () => {
+  return (
+    
+    <div>
+        <Navbar/>
+        SearchPage
+    </div>
+  )
+}
+
+export default SearchPage
