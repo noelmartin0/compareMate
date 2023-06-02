7acc41f392b53e19d2ef8f5fcf669192d3e8e126
Noel Martin <noelmartin0@gmail.com>
2023-06-02 14:45:08 +0530

    footer fix

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../components/navbar'
-import Footer from '../components/footer'
+import Footer from '../components/Footer'
 import ItemCard from '../components/ItemCard'
 import styled from 'styled-components'
 
